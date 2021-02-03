b21c73ff31384b096243847b06d427437e56599e
Anastasiya <pustoe0@gmail.com>
2021-02-03 13:38:19 +0300

    project files added

---
+++ script.js
@@ -0,0 +1,56 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const forms = document.querySelectorAll(".form-check");
+  const blockContent = document.querySelector(".menu-options__block");
+  const data = {};
+  forms.forEach((form, index) => {
+    const dataForm = form.dataset.item;
+    let content = "";
+    data[dataForm] = [];
+    const inputs = form.querySelectorAll("input");
+    inputs.forEach((input) => {
+      input.onchange = function () {
+        const value = input.value;
+        const arrow = document.querySelectorAll(".list-item")[index];
+        if (
+          (input.type == "checkbox" && input.checked === false) ||
+          input.value === ""
+        ) {
+          const indexRemove = data[dataForm].indexOf(value);
+          data[dataForm].splice(indexRemove, 1);
+          if (data[dataForm].length == 0) {
+            arrow.classList.remove("decor");
+          }
+        } else if (input.value != "" || input.checked === true) {
+          arrow.classList.add("decor");
+
+          if (dataForm === "diameter") {
+            data[dataForm] = [];
+            data[dataForm].push(value);
+          } else {
+            data[dataForm].push(value);
+          }
+        }
+
+        content = "";
+        for (let key in data) {
+          if (key === "diameter" && data[key].length !== 0) {
+            if (+value >= 4 && +value <= 98) {
+              content = "Диаметр " + data[key] + ",";
+            } else {
+              input.value = "";
+            }
+          } else {
+            content += data[key] + " ";
+          }
+        }
+        if (content != "    ") {
+          blockContent.textContent = content.split(",").join(", ");
+          blockContent.style.color = "#898989";
+        } else {
+          blockContent.textContent = "Выберете нужные параметры";
+          blockContent.style.color = "#3d3d3d";
+        }
+      };
+    });
+  });
+});
